1e6743d84f472e952ba0621ce8870fe552838423
fjoegh <67545244+fjoegh@users.noreply.github.com>
2021-02-11 12:16:13 +0000

    Create hm4.1.py

---
+++ hm4.1.py
@@ -0,0 +1,21 @@
+i = 0
+for num in range(1,101):
+#    for i in num:
+#       i = iter(num)   
+#        print(num)
+        if num % i == 0 and num % 1 == num:
+           print ("Prime")
+           break
+        else:
+            print(num)
+"""       if num%3 == 0:
+          print("Fizz")
+          continue
+          if num%5 == 0:
+              print("Buzz")
+              continue
+              if num%15 == 0:
+                  print("fizzBuzz")
+                  break
+print(num)"""
+                    
